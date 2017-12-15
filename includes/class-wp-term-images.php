ed45ad7f273f3704b0ea666595700d4e98767372
Jonny Harris <jon@spacedmonkey.co.uk>
2017-12-15 11:47:52 +0000

    Code formatting changes after running PHP code sniffer.

--- includes/class-wp-term-images.php
+++ includes/class-wp-term-images.php
@@ -12,113 +12,120 @@
 defined( 'ABSPATH' ) || exit;
 
 if ( ! class_exists( 'WP_Term_Images' ) ) :
-/**
+	/**
  * Main WP Term Images class
  *
  * @since 0.1.0
  */
-final class WP_Term_Images extends WP_Term_Meta_UI {
+	final class WP_Term_Images extends WP_Term_Meta_UI {
 
-	/**
+		/**
 	 * @var string Plugin version
 	 */
-	public $version = '1.0.0';
+		public $version = '1.0.0';
 
-	/**
+		/**
 	 * @var string Database version
 	 */
-	public $db_version = 201701160001;
+		public $db_version = 201701160001;
 
-	/**
+		/**
 	 * @var string Metadata key
 	 */
-	public $meta_key = 'image';
+		public $meta_key = 'image';
 
-	/**
+		/**
 	 * Hook into queries, admin screens, and more!
 	 *
 	 * @since 0.1.0
 	 */
-	public function __construct( $file = '' ) {
+		public function __construct( $file = '' ) {
 
-		// Setup the labels
-		$this->labels = array(
-			'singular'    => esc_html__( 'Image',  'wp-term-images' ),
-			'plural'      => esc_html__( 'Images', 'wp-term-images' ),
-			'description' => esc_html__( 'Assign terms a custom image to visually separate them from each-other.', 'wp-term-images' )
-		);
+			// Setup the labels
+			$this->labels = array(
+				'singular'    => esc_html__( 'Image',  'wp-term-images' ),
+				'plural'      => esc_html__( 'Images', 'wp-term-images' ),
+				'description' => esc_html__( 'Assign terms a custom image to visually separate them from each-other.', 'wp-term-images' ),
+			);
 
-		// Translations
-		load_plugin_textdomain( 'wp-term-images' );
+			// Translations
+			load_plugin_textdomain( 'wp-term-images' );
 
-		// Call the parent and pass the file
-		parent::__construct( $file );
-	}
+			// Call the parent and pass the file
+			parent::__construct( $file );
+		}
 
-	/** Assets ****************************************************************/
+		/** Assets ****************************************************************/
 
-	/**
+		/**
 	 * Enqueue quick-edit JS
 	 *
 	 * @since 0.1.0
 	 */
-	public function enqueue_scripts() {
+		public function enqueue_scripts() {
 
-		// Enqueue media
-		wp_enqueue_media();
+			// Enqueue media
+			wp_enqueue_media();
 
-		// Enqueue media handler; includes quick-edit
-		wp_enqueue_style( 'wp-term-images',  $this->url . 'assets/css/term-image.css', array(),           $this->db_version       );
-		wp_enqueue_script( 'wp-term-images', $this->url . 'assets/js/term-image.js',   array( 'jquery' ), $this->db_version, true );
+			// Enqueue media handler; includes quick-edit
+			wp_enqueue_style( 'wp-term-images',  $this->url . 'assets/css/term-image.css', array(),           $this->db_version );
+			wp_enqueue_script( 'wp-term-images', $this->url . 'assets/js/term-image.js',   array( 'jquery' ), $this->db_version, true );
 
-		// Term ID
-		$term_id = ! empty( $_GET['tag_ID'] )
-			? (int) $_GET['tag_ID']
-			: 0;
+			// Term ID
+			$term_id = 0;
+			// @codingStandardsIgnoreStart
+			if ( isset( $_GET['tag_ID'] ) ) {
+				$term_id = (int) sanitize_text_field( wp_unslash( $_GET['tag_ID'] ) );
+			}
+			// @codingStandardsIgnoreEnd
 
-		// Localize
-		wp_localize_script( 'wp-term-images', 'i10n_WPTermImages', array(
-			'insertMediaTitle' => esc_html__( 'Choose an Image', 'wp-term-images' ),
-			'insertIntoPost'   => esc_html__( 'Set as image',    'wp-term-images' ),
-			'deleteNonce'      => wp_create_nonce( 'remove_wp_term_images_nonce' ),
-			'mediaNonce'       => wp_create_nonce( 'assign_wp_term_images_nonce' ),
-			'term_id'          => $term_id,
-		) );
-	}
+			// Localize
+			wp_localize_script(
+				'wp-term-images', 'i10n_WPTermImages', array(
+					'insertMediaTitle' => esc_html__( 'Choose an Image', 'wp-term-images' ),
+					'insertIntoPost'   => esc_html__( 'Set as image',    'wp-term-images' ),
+					'deleteNonce'      => wp_create_nonce( 'remove_wp_term_images_nonce' ),
+					'mediaNonce'       => wp_create_nonce( 'assign_wp_term_images_nonce' ),
+					'term_id'          => $term_id,
+				)
+			);
+		}
 
-	/**
+		/**
 	 * Add help tabs for `image` column
 	 *
 	 * @since 0.1.2
 	 */
-	public function help_tabs() {
-		get_current_screen()->add_help_tab(array(
-			'id'      => 'wp_term_image_help_tab',
-			'title'   => __( 'Term Image', 'wp-term-images' ),
-			'content' => '<p>' . __( 'Terms can have unique images to help separate them from each other.', 'wp-term-images' ) . '</p>',
-		) );
-	}
+		public function help_tabs() {
+			get_current_screen()->add_help_tab(
+				array(
+					'id'      => 'wp_term_image_help_tab',
+					'title'   => __( 'Term Image', 'wp-term-images' ),
+					'content' => '<p>' . __( 'Terms can have unique images to help separate them from each other.', 'wp-term-images' ) . '</p>',
+				)
+			);
+		}
 
-	/**
+		/**
 	 * Return the formatted output for the colomn row
 	 *
 	 * @since 0.1.2
 	 *
 	 * @param string $meta
 	 */
-	protected function format_output( $meta = '' ) {
+		protected function format_output( $meta = '' ) {
 
-		// Filter image attributes and add the attachment ID
-		add_filter( 'wp_get_attachment_image_attributes', array( $this, 'attachment_id_attr' ), 10, 2 );
+			// Filter image attributes and add the attachment ID
+			add_filter( 'wp_get_attachment_image_attributes', array( $this, 'attachment_id_attr' ), 10, 2 );
 
-		// Output the image attachment
-		echo wp_get_attachment_image( $meta );
+			// Output the image attachment
+			echo wp_get_attachment_image( $meta );
 
-		// Remove our filter
-		remove_filter( 'wp_get_attachment_image_attributes', array( $this, 'attachment_id_attr' ), 10, 2 );
-	}
+			// Remove our filter
+			remove_filter( 'wp_get_attachment_image_attributes', array( $this, 'attachment_id_attr' ), 10, 2 );
+		}
 
-	/**
+		/**
 	 * Add attachment ID as data attribute, used by Quick Edit
 	 *
 	 * @since 0.1.3
@@ -127,64 +134,66 @@ final class WP_Term_Images extends WP_Term_Meta_UI {
 	 * @param int   $attachment
 	 * @param int   $size
 	 */
-	public static function attachment_id_attr( $attr = array(), $attachment = 0 ) {
-		$attr['data-attachment-id'] = $attachment->ID;
-		return $attr;
-	}
+		public static function attachment_id_attr( $attr = array(), $attachment = 0 ) {
+			$attr['data-attachment-id'] = $attachment->ID;
+			return $attr;
+		}
 
-	/**
+		/**
 	 * Output the form field
 	 *
 	 * @since 0.1.0
 	 *
 	 * @param  $term
 	 */
-	protected function form_field( $term = '' ) {
+		protected function form_field( $term = '' ) {
 
-		$term_id = ! empty( $term->term_id )
+			$term_id = ! empty( $term->term_id )
 			? $term->term_id
 			: 0;
 
-		// Remove image URL
-		$remove_url = add_query_arg( array(
-			'action'   => 'remove-wp-term-images',
-			'term_id'  => $term_id,
-			'_wpnonce' => false,
-		) );
+			// Remove image URL
+			$remove_url = add_query_arg(
+				array(
+					'action'   => 'remove-wp-term-images',
+					'term_id'  => $term_id,
+					'_wpnonce' => false,
+				)
+			);
 
-		// Get the meta value
-		$value  = $this->get_meta( $term_id );
-		$hidden = empty( $value )
-			? ' style="display: none;"'
+			// Get the meta value
+			$value  = $this->get_meta( $term_id );
+			$hidden = empty( $value )
+			? ' display: none; '
 			: ''; ?>
 
-		<div>
-			<img id="wp-term-images-photo" src="<?php echo esc_url( wp_get_attachment_image_url( $value, 'full' ) ); ?>"<?php echo $hidden; ?> />
-			<input type="text" style="display: none;" name="term-<?php echo esc_attr( $this->meta_key ); ?>" id="term-<?php echo esc_attr( $this->meta_key ); ?>" value="<?php echo esc_attr( $value ); ?>" />
-		</div>
+			<div>
+				<img id="wp-term-images-photo" src="<?php echo esc_url( wp_get_attachment_image_url( $value, 'full' ) ); ?>" style="<?php echo esc_attr( $hidden ); ?>" />
+				<input type="text" style="display: none;" name="term-<?php echo esc_attr( $this->meta_key ); ?>" id="term-<?php echo esc_attr( $this->meta_key ); ?>" value="<?php echo esc_attr( $value ); ?>" />
+			</div>
 
-		<a class="button-secondary wp-term-images-media">
-			<?php esc_html_e( 'Choose Image', 'wp-term-images' ); ?>
-		</a>
+			<a class="button-secondary wp-term-images-media">
+				<?php esc_html_e( 'Choose Image', 'wp-term-images' ); ?>
+			</a>
 
-		<a href="<?php echo esc_url( $remove_url ); ?>" class="button wp-term-images-remove"<?php echo $hidden; ?>>
-			<?php esc_html_e( 'Remove', 'wp-term-images' ); ?>
-		</a>
+			<a href="<?php echo esc_url( $remove_url ); ?>" class="button wp-term-images-remove" style="<?php echo esc_attr( $hidden ); ?>">
+				<?php esc_html_e( 'Remove', 'wp-term-images' ); ?>
+			</a>
 
-		<?php
-	}
+			<?php
+		}
 
-	/**
+		/**
 	 * Output the form field
 	 *
 	 * @since 0.1.0
 	 *
 	 * @param  $term
 	 */
-	protected function quick_edit_form_field() {
-		?>
+		protected function quick_edit_form_field() {
+			?>
 
-		<input type="hidden" name="term-<?php echo esc_attr( $this->meta_key ); ?>" value="">
+			<input type="hidden" name="term-<?php echo esc_attr( $this->meta_key ); ?>" value="">
 		<button class="button wp-term-images-media quick">
 			<?php esc_html_e( 'Choose Image', 'wp-term-images' ); ?>
 		</button>
@@ -194,6 +203,6 @@ final class WP_Term_Images extends WP_Term_Meta_UI {
 		</a>
 
 		<?php
+		}
 	}
-}
 endif;
