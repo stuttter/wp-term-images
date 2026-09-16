7cd5c291e301a3bd6e08cb6dcb5abc03d5ca91db
John James Jacoby <johnjamesjacoby@me.com>
2026-09-16 15:27:45 -0500

    Release: Prepare version 2.2.0.
    Promote the merged REST exposure, taxonomy targeting, and enforceable compatibility tooling into the next feature release.

--- wp-term-images.php
+++ wp-term-images.php
@@ -8,7 +8,7 @@
  * License:     GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Description: Pretty images for categories, tags, and other taxonomy terms
- * Version:     2.1.3
+ * Version:     2.2.0
  * Requires at least: 6.4
  * Requires PHP: 7.4
  * Text Domain: wp-term-images
